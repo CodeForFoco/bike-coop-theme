98c5c787b19dfca11e7f881b6dd81b25eee78f7e
Jason Lampel <travelampel@gmail.com>
2016-09-10 17:34:03 -0600

    Add logo to page header/title; move hamburger menu to right side.

--- header.php
+++ header.php
@@ -29,16 +29,27 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="title-bar" data-responsive-toggle="site-navigation">
-			<button class="menu-icon" type="button" data-toggle="mobile-menu"></button>
 			<div class="title-bar-title">
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+					<?php
+						echo file_get_contents(get_template_directory_uri() . '/assets/images/icons/fcbc-logo.svg' );
+					?>
+					<span><?php bloginfo( 'name' ); ?></span>
+				</a>
+				<button class="menu-icon" type="button" data-toggle="mobile-menu"></button>
 			</div>
 		</div>
-
 		<nav id="site-navigation" class="main-navigation top-bar" role="navigation">
 			<div class="top-bar-left">
 				<ul class="menu">
-					<li class="home"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></li>
+					<li class="home">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+							<?php
+								echo file_get_contents(get_template_directory_uri() . '/assets/images/icons/fcbc-logo.svg' );
+								bloginfo( 'name' );
+							?>
+						</a>
+					</li>
 				</ul>
 			</div>
 			<div class="top-bar-right">
