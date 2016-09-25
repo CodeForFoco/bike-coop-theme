a354a4f86fdc06549067735388a8813d7d7b9f2a
Jason Lampel <travelampel@gmail.com>
2016-09-25 11:35:53 -0600

    Add class to footer widget headings.

--- library/widget-areas.php
+++ library/widget-areas.php
@@ -14,7 +14,7 @@ function foundationpress_sidebar_widgets() {
 	  'description' => __( 'Drag widgets to this sidebar container.', 'foundationpress' ),
 	  'before_widget' => '<article id="%1$s" class="widget %2$s">',
 	  'after_widget' => '</article>',
-	  'before_title' => '<h6>',
+	  'before_title' => '<h6 class="widget__heading">',
 	  'after_title' => '</h6>',
 	));
 
@@ -24,7 +24,7 @@ function foundationpress_sidebar_widgets() {
 	  'description' => __( 'Drag widgets to this footer container', 'foundationpress' ),
 	  'before_widget' => '<article id="%1$s" class="large-4 columns widget %2$s">',
 	  'after_widget' => '</article>',
-	  'before_title' => '<h6>',
+	  'before_title' => '<h6 class="widget__heading">',
 	  'after_title' => '</h6>',
 	));
 }
