94ca3360ad9ae5fa56abffcd35571ea572a948f4
Jason Lampel <travelampel@gmail.com>
2016-09-29 18:25:12 -0600

    Migrated footer widgets into code.

--- footer.php
+++ footer.php
@@ -15,6 +15,74 @@
 			<footer id="footer" class="footer">
 				<?php do_action( 'foundationpress_before_footer' ); ?>
 				<?php dynamic_sidebar( 'footer-widgets' ); ?>
+				<article class="column medium-4">
+					<h3 class="footer__heading">Hours of Operation</h3>
+					<b>PUBLIC, OPEN-SHOP HOURS</b>
+					<table class="footer__table">
+					    <tbody>
+					        <tr>
+					            <td>Sunday</td>
+					            <td>12-6pm</td>
+					        </tr>
+					        <tr>
+					            <td>Monday</td>
+					            <td>2-5pm</td>
+					        </tr>
+					        <tr>
+					            <td>Tuesday</td>
+					            <td>Closed</td>
+					        </tr>
+					        <tr>
+					            <td>Wednesday</td>
+					            <td>2-5pm</td>
+					        </tr>
+					        <tr>
+					            <td>Thursday</td>
+					            <td>Closed</td>
+					        </tr>
+					        <tr>
+					            <td>Friday</td>
+					            <td>2-5pm</td>
+					        </tr>
+					        <tr>
+					            <td>Saturday</td>
+					            <td>Closed</td>
+					        </tr>
+					    </tbody>
+					</table>
+					<b>VOLUNTEER WORK NIGHTS</b>
+					<table class="footer__table">
+					    <tbody>
+					        <tr>
+					            <td>Monday</td>
+					            <td>6-10pm</td>
+					        </tr>
+					        <tr>
+					            <td>Wednesday</td>
+					            <td>6-10pm</td>
+					        </tr>
+					        <tr>
+					            <td>Friday</td>
+					            <td>6-10pm</td>
+					        </tr>
+					    </tbody>
+					</table>
+				</article>
+				<article class="column medium-4">
+					<h3 class="footer__heading">Donate</h3>
+					<p>Donations always welcome!</p>
+					<a role="button" class="download large button sites-button" href="/donate">Donate to FCBC</a>
+				</article>
+				<article class="column medium-4">
+					<h3 class="footer__heading">Contact Us</h3>
+					<p>
+					    1501 North College Avenue
+					    <br> Fort Collins, CO 80524
+					    <br> (970) 484-3804
+					</p>
+					<a href="https://www.facebook.com/FortCollinsBikeCoOp" target="_blank"><i class="fa fa-3x fa-facebook-square"></i></a>
+					<a href="https://twitter.com/fcbikecoop" target="_blank"><i class="fa fa-3x fa-twitter-square"></i></a>
+				</article>
 				<?php do_action( 'foundationpress_after_footer' ); ?>
 			</footer>
 		</div>
