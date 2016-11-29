ebf3b399f6d1b968daffebe0d1c4ee9d8286244a
Jason Lampel <travelampel@gmail.com>
2016-11-29 08:39:26 -0700

    Home page: convert wheels to links

--- page-templates/front.php
+++ page-templates/front.php
@@ -24,14 +24,20 @@ get_header(); ?>
       <div class="entry-content">
         <?php the_content(); ?>
         <div class="row wheel-backgrounds">
-          <div class="column medium-4 wheel-bg--bike-shop">
-            <?php echo file_get_contents(get_stylesheet_directory().'/assets/images/icons/wheel-icon.svg'); ?>
+          <div class="column medium-4 wheel-bg--bike-shop" title="Bike shop parts, sales, and service">
+            <a href="bike-shop">
+              <?php echo file_get_contents(get_stylesheet_directory().'/assets/images/icons/wheel-icon.svg'); ?>
+            </a>
           </div>
-          <div class="column medium-4 wheel-bg--programs">
-            <?php echo file_get_contents(get_stylesheet_directory().'/assets/images/icons/wheel-icon.svg'); ?>
+          <div class="column medium-4 wheel-bg--programs" title="Bike co-op programs">
+            <a href="programs">
+              <?php echo file_get_contents(get_stylesheet_directory().'/assets/images/icons/wheel-icon.svg'); ?>
+            </a>
           </div>
-          <div class="column medium-4 wheel-bg--volunteer">
-            <?php echo file_get_contents(get_stylesheet_directory().'/assets/images/icons/wheel-icon.svg'); ?>
+          <div class="column medium-4 wheel-bg--volunteer" title="Volunteer application form">
+            <a href="volunteer">
+              <?php echo file_get_contents(get_stylesheet_directory().'/assets/images/icons/wheel-icon.svg'); ?>
+            </a>
           </div>
         </div>
       </div>
